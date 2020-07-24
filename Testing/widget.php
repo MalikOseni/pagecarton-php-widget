1569f9e788beb733b50d016135e815208e03bcee
Malik Adeoye Oseni <57351965+MalikOseni@users.noreply.github.com>
2020-07-24 15:47:27 +0100

    Update widget.php

--- Testing/widget.php
+++ Testing/widget.php
@@ -69,37 +69,3 @@ class Testing_widget extends PageCarton_Widget
 
 
 
-<div class="section">
-    <div class="container">
-      <div class="row align-items-center">
-
-        <div class="col-md-6 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-          <figure class="img-dotted-bg"><img src="/layout/pc_layout_inova/images/about_2.jpg" alt="Image" class="img-fluid"></figure>
-</div>
-        <div class="col-md-5 ml-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="">
-          <h2 class="mb-4 section-title">History of &nbsp; {Organization Name}</h2>
-          <p>{Background History Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt labore et magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea consequat.}</p>
-          
-        </div>
-      </div>
-
-    </div>
-  </div>
-
-
-
-
-  <div class="section">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-6 order-md-2 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-                <figure class="img-dotted-bg"><img src="/layout/pc_layout_inova/images/hero_2.jpg" alt="Image" class="img-fluid"></figure>
-            </div>
-            <div class="col-md-5 mr-auto aos-init aos-animate" data-aos="fade-up" data-aos-delay="">
-                <h2 class="mb-4 section-title">About &nbsp;{Organization Name}</h2>
-                <p>{Short About Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius repellat, dicta at laboriosam, nemo exercitationem itaque eveniet architecto cumque, deleniti commodi molestias repellendus quos sequi hic fugiat asperiores illum. Atque, in, fuga excepturi corrupti error corporis aliquam unde nostrum quas.}</p>
-                <p>{More About Accusantium dolor ratione maiores est deleniti nihil? Dignissimos est, sunt nulla illum autem in, quibusdam cumque recusandae, laudantium minima repellendus.} </p>
-            </div>
-        </div>
-    </div>
-</div>
